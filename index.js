2959274c21098a59fb642568b892cf2868cb8a33
liugd <ixiaoyver@gmail.com>
2020-09-12 22:28:05 +0800

    微调

--- index.js
+++ index.js
@@ -15,11 +15,12 @@ app.post(url + "/user/login", function (req, res) {
   res.json(require("./data/user/login.json"));
 });
 
-// 这段在最后，请求错误时返回
+// 这段在最后，在请求错误时返回
 app.post("*", function (req, res) {
   log.printLog(req);
   res.json(require("./data/other/404.json"));
 });
+
 app.get("*", function (req, res) {
   log.printLog(req);
   res.json(require("./data/other/404.json"));
